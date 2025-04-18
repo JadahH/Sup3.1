8a3319aef53992541846258e5ddd44eace164fed
JadahH <Jhol1918@students.vsu.edu>
2025-04-18 19:10:09 -0400

    Added test for triangle_area

--- test/test_calculation.py
+++ test/test_calculation.py
@@ -1,4 +1,4 @@
-from area_calculation.calculation import rectangle_area
+from area_calculation.calculation import rectangle_area, triangle_area
 
 
 def test_rectangle_of_different_dimmensions():
@@ -6,3 +6,6 @@ def test_rectangle_of_different_dimmensions():
 
 def test_rectangle_of_same_dimmensions():
     assert rectangle_area(5,5) == 25
+
+def test_triangle_area():
+    assert triangle_area(5,4) == 10

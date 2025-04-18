4ca306b622a3f2f59001edb55677a7c8305e235e
JadahH <Jhol1918@students.vsu.edu>
2025-04-18 19:36:56 -0400

    added test for circle area

--- test/test_calculation.py
+++ test/test_calculation.py
@@ -1,4 +1,4 @@
-from area_calculation.calculation import rectangle_area, triangle_area
+from area_calculation.calculation import rectangle_area, triangle_area, circle_area
 
 
 def test_rectangle_of_different_dimmensions():
@@ -9,3 +9,6 @@ def test_rectangle_of_same_dimmensions():
 
 def test_triangle_area():
     assert triangle_area(5,4) == 10
+
+def test_cirle_area():
+    assert circle_area(4) == 12.56
